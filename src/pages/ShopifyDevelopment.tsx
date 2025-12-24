cb707b216aadc8234663911e73410abe40ffa65c
Aashif <masaashifsajah@gmail.com>
2025-12-24 21:56:22 +0530

    Updated the favicon

--- src/pages/ShopifyDevelopment.tsx
+++ src/pages/ShopifyDevelopment.tsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
+                                                                                                                                                                                                                                   import { Link } from "react-router-dom";import { Helmet } from "react-helmet-async";
 import { 
   ArrowRight, 
   Check, 
